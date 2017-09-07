b733bcd6c30d1064b26c3bf751e5f9ac839c7665
Dmytro Andreikiv <dmytro.andreikiv@philips.com>
2017-09-07 17:46:48 +0200

    Removed unneeded files.

--- Playground/ObfuscatedString.playground/Contents.swift
+++ Playground/ObfuscatedString.playground/Contents.swift
@@ -2,7 +2,7 @@ import Foundation
 
 /*:
 ## Overview
-- According to the assignement there was a requiremnt to implement a class which takes a string and creates an obfuscated version of it. The **ObfuscatedString** wrapper doesn't make a copy of an original string. It takes a string and crated an obfuscated string out of it and stores it internaly.
+- According to the assignement there was a requiremnt to implement a class which takes a string and creates an obfuscated version of it. The **ObfuscatedString** wrapper doesn't make a copy of an original string. It takes a string and crates an obfuscated verison out of it and stores it internaly.
 
 - During implementation it was decided to use [Strategy Design Pattern](https://en.wikipedia.org/wiki/Strategy_pattern) which gives a possibility to use different algorithms for string obfuscation.
 */
